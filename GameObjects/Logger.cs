88ecf1790107b61c9b292319747208d6ca2bd2a8
Michael <vo1kod4v@gmail.com>
2023-08-05 17:56:59 +0300

    Logger now works in separate thread and does not impact GameLoop performance Logger can now also write into a CSV file.

--- GameObjects/Logger.cs
+++ GameObjects/Logger.cs
@@ -1,59 +1,145 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.IO;
+using System.Threading;
 
 namespace GameObjects
 {
-
-    public enum LogLevel {Nothing, Status, Info, Warning, Debug, All }
-
-    public static class Logger
+    public struct LogMsg
     {
+        private string _message;
+        public Exception Exception;
+        public LogLevel loglevel;
+        public string Message
+        {
+            get
+            {
+                if (Exception is null)
+                {
+                    return _message;
+                }
+                return Exception.Message;
+            }
+            set { _message = value; }
+        }
+    }
+
+    public enum LogLevel {Nothing, Status, Info, Warning, Debug, CSV, All }
+
+    public class Logger
+    {
+
+        private BlockingCollection<LogMsg> messages;
+        private Thread T;
+        private TextWriter output { get; set; }
+        private readonly static Lazy<Logger> _instance = new Lazy<Logger>(() => new Logger());
+
+        public static Logger Instance
+        {
+            get
+            {
+                return _instance.Value;
+            }
+        }
 
         private static List<LogLevel> loglevels { get; set; } = new List<LogLevel>()
         {
             LogLevel.Debug,
             LogLevel.Info,
-            LogLevel.Status
+            LogLevel.Status,
+            LogLevel.CSV
         };
 
+        public Logger()
+        {
+            T = new Thread(Process);
+            T.IsBackground = true;
+            T.Name = "Logger";
+            messages = new BlockingCollection<LogMsg>();
+            output = Console.Out;
+            T.Start();
+        }
+
+        public void Init(string filepath)
+        {
+            // StreamWriter outputFile = outputFile;
+            output = new StreamWriter(filepath); 
+        }
+
+        public static void Process()
+        {
+            foreach (var message in Instance.messages.GetConsumingEnumerable())
+            {
+                Write(message);
+            }
+            Instance.messages.Dispose();
+        }
+
+
+
+
+
         public static void Log(Exception exception, LogLevel ll)
+        {
+            if (!loglevels.Contains(ll))
+                return;
+            Instance.messages.Add(new LogMsg{Exception = exception, loglevel = ll});
+        }
+
+        public static void Log(string message, LogLevel ll)
         {
 
             if (!loglevels.Contains(ll))
                 return;
+            if (ll is LogLevel.Debug)
+            {
+                Exception exception;
+                try
+                {
+                    throw new Exception();
+                }
+                catch (Exception ex)
+                {
+                    exception = new Exception(message, ex);
+                }
+                Log(exception, ll);
+            }
+            else
+            {
+                Instance.messages.Add(new LogMsg { Message = message, loglevel = ll });
+            }
+        }
 
-            switch (ll)
+        public static void Write(LogMsg msg)
+        {
+            switch (msg.loglevel)
             {
                 case LogLevel.Status:
-                    Console.Write('\r' + exception.Message);
+                    Instance.output.Write('\r' + msg.Message);
                     break;
                 case LogLevel.Info:
-                    Console.WriteLine(exception.Message);
+                    Instance.output.WriteLine(msg.Message);
                     break;
                 case LogLevel.Warning:
-                    Console.WriteLine(exception.Message);
+                    Instance.output.WriteLine(msg.Message);
                     break;
                 case LogLevel.Debug:
-                    Console.WriteLine(exception.Message);
-                    Console.WriteLine(exception.StackTrace);
+                    Instance.output.WriteLine(msg.Message);
+                    Instance.output.WriteLine(msg.Exception.StackTrace);
+                    break;
+                case LogLevel.CSV:
+                    Instance.output.WriteLine(msg.Message);
                     break;
                 default:
                     break;
             }
         }
 
-        public static void Log(string message, LogLevel ll)
-        {
-            Exception exception;
-            try
-            {
-                throw new Exception();
-            }
-            catch (Exception ex)
-            {
-                exception = new Exception(message, ex);
-            }            
-            Log(exception, ll);
+        public void Dispose()
+        {            
+            messages.CompleteAdding();
+            output.Dispose();
         }
     }
 }
